d97a7f2d7a1d48be4059ae95006d1d5fd9470379
filipchelkowski <filip.chelkowski@gmail.com>
2026-03-03 18:40:27 +0100

    Update FileReaderImplTest.java

--- src/test/java/core/basesyntax/service/impl/FileReaderImplTest.java
+++ src/test/java/core/basesyntax/service/impl/FileReaderImplTest.java
@@ -1,10 +1,13 @@
 package core.basesyntax.service.impl;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import core.basesyntax.service.FileReader;
 import java.util.List;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class FileReaderImplTest {
@@ -13,14 +16,24 @@ class FileReaderImplTest {
     private FileReader fileReader = new FileReaderImpl();
 
     @Test
-    void createsListOfTransactions_Ok() {
+    void read_createsListOfTransactions_ok() {
         List<String> result = fileReader.read(testFile);
+        int expected = 3;
+        int actual = result.size();
         assertNotNull(result);
-
+        assertEquals(expected, actual);
     }
 
     @Test
-    void throwsException_Ok() {
+    void read_parseLines_ok() {
+        List<String> result = fileReader.read(testFile);
+        String expected = "b,banana,20";
+        String actual = result.get(1);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void read_incorrectPath_throwsException_Ok() {
         assertThrows(RuntimeException.class, () -> fileReader.read(invalidTestFile));
     }
 }
