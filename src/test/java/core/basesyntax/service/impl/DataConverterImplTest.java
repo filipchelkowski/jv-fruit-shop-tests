2b733e4e3088aea5acce066fa627a39ad84b4bbe
filipchelkowski <filip.chelkowski@gmail.com>
2026-03-05 19:09:22 +0100

    Update DataConverterImplTest.java

--- src/test/java/core/basesyntax/service/impl/DataConverterImplTest.java
+++ src/test/java/core/basesyntax/service/impl/DataConverterImplTest.java
@@ -14,11 +14,20 @@ class DataConverterImplTest {
     private DataConverter dataConverter = new DataConverterImpl();
     private List<String> listOfTransactions = List.of("op,furit,qty",
             "b,apple,10");
-    private List<String> invalidListOfTransactions = List.of("op.furit.qty",
+    private List<String> invalidListOfTransactionsWrongSeparator = List.of("op.furit.qty",
             "b.apple.10");
+    private List<String> invalidListOfTransactionsMoreColumns = List.of("op,furit,qty,additionl",
+            "b,apple,10,extra");
+    private List<String> invalidListOfTransactionsLessColumns = List.of("op,furit",
+            "b,apple");
+    private List<String> invalidListOfTransactionsDoubleQuantity = List.of("op,furit,qty",
+            "b,apple,20.8");
+    private List<String> invalidListOfTransactionsEmpty = List.of("");
+    private List<String> invalidListOfTransactionsIncorrectOperation = List.of("op,furit,qty",
+            "z,apple,10");
 
     @Test
-    void convertToTransactions_validData_ok() {
+    void convertToTransactions_validData_returnList_ok() {
         List<FruitTransaction> result = dataConverter.convertToTransactions(listOfTransactions);
         int expected = listOfTransactions.size() - 1;
         int actual = result.size();
@@ -27,7 +36,7 @@ class DataConverterImplTest {
     }
 
     @Test
-    void convertToTransactions_parseFruitName_ok() {
+    void convertToTransactions_validData_parseFruitName_ok() {
         List<FruitTransaction> result = dataConverter.convertToTransactions(listOfTransactions);
         String expected = "apple";
         String actual = result.get(0).getFruit();
@@ -35,7 +44,7 @@ class DataConverterImplTest {
     }
 
     @Test
-    void convertToTransactions_parseOperation_ok() {
+    void convertToTransactions_validData_parseOperation_ok() {
         List<FruitTransaction> result = dataConverter.convertToTransactions(listOfTransactions);
         FruitTransaction.Operation expected = FruitTransaction.Operation.BALANCE;
         FruitTransaction.Operation actual = result.get(0).getOperation();
@@ -43,7 +52,7 @@ class DataConverterImplTest {
     }
 
     @Test
-    void convertToTransactions_parseQuantity_ok() {
+    void convertToTransactions_validData_parseQuantity_ok() {
         List<FruitTransaction> result = dataConverter.convertToTransactions(listOfTransactions);
         int expected = 10;
         int actual = result.get(0).getQuantity();
@@ -51,9 +60,45 @@ class DataConverterImplTest {
     }
 
     @Test
-    void convertToTransactions_invalidSeparator_throwException() {
+    void convertToTransactions_invalidData_invalidSeparator_notOk() {
         assertThrows(RuntimeException.class,
-                () -> dataConverter.convertToTransactions(invalidListOfTransactions));
+                () -> dataConverter.convertToTransactions(invalidListOfTransactionsWrongSeparator));
+
+    }
+
+    @Test
+    void convertToTransactions_invalidData_moreColumns_notOk() {
+        assertThrows(RuntimeException.class,
+                () -> dataConverter.convertToTransactions(invalidListOfTransactionsMoreColumns));
+
+    }
+
+    @Test
+    void convertToTransactions_invalidData_lessColumns_notOk() {
+        assertThrows(RuntimeException.class,
+                () -> dataConverter.convertToTransactions(invalidListOfTransactionsLessColumns));
+
+    }
+
+    @Test
+    void convertToTransactions_invalidData_doubleInsteadOfInt_notOk() {
+        assertThrows(RuntimeException.class,
+                () -> dataConverter.convertToTransactions(invalidListOfTransactionsDoubleQuantity));
+
+    }
+
+    @Test
+    void convertToTransactions_invalidData_emptyInput_notOk() {
+        assertThrows(RuntimeException.class,
+                () -> dataConverter.convertToTransactions(invalidListOfTransactionsEmpty));
+
+    }
+
+    @Test
+    void convertToTransactions_invalidData_incorrectOperationCode_notOk() {
+        assertThrows(RuntimeException.class,
+                () -> dataConverter.convertToTransactions(
+                        invalidListOfTransactionsIncorrectOperation));
 
     }
 }
